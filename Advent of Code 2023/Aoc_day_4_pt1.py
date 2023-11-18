9770da17bfe049c743aebb29dc5f81bcfa2e6170
sachinsujir <sujirsachin@gmail.com>
2023-11-18 13:50:22 -0500

    AoC day 4

---
+++ Advent of Code 2023/Aoc_day_4_pt1.py
@@ -0,0 +1,21 @@
+def find_count_of_duplicates(assignment_pairs) -> int:
+    count = 0
+    j = 0
+    for pair in assignment_pairs:
+        sections = [list(map(int, section.split('-'))) for section in pair.split(',')]
+        if is_subset_section(list(range(sections[j][0], sections[j][1] + 1)),
+                             list(range(sections[j + 1][0], sections[j + 1][1] + 1))):
+            count += 1
+    return count
+
+
+def is_subset_section(elf1, elf2):
+    return set(elf1).issubset(set(elf2)) or set(elf2).issubset(set(elf1))
+
+
+with open("day4Input1.txt", "r") as f:
+    assignment_pairs = f.readlines()
+
+assignment_pairs = [assignment.replace("\n", "") for assignment in assignment_pairs]
+assignment_pairs2 = ["2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"]
+print(find_count_of_duplicates(assignment_pairs))
